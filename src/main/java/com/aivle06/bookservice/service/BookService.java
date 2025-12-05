db1761f84c153ddda9404cc79338634c7174886f
simon <a081127@naver.com>
2025-12-05 11:28:07 +0900

    Add BookServiceImpl initial commit

--- src/main/java/com/aivle06/bookservice/service/BookService.java
+++ src/main/java/com/aivle06/bookservice/service/BookService.java
@@ -7,7 +7,8 @@ import java.util.List;
 public interface BookService {
 
     // 저장
-    Long createBook(Book book);
+    Book createBook(Book book);
+    //implements에서 충돌해서 Long에서 Book으로 다시 바꿨습니다
 
     // 단건 조회
     Book getBookById(Long id);
@@ -16,7 +17,8 @@ public interface BookService {
     List<Book> getAllBooks();
 
     // 업데이트
-    void updateBook(Long id, Book bookDetails);
+    Book updateBook(Long id, Book bookDetails);
+    //저장하면서 Book이 return되면서 void랑 충돌했습니다;;
 
     // 삭제
     void deleteBook(Long id);
---
+++ src/main/java/com/aivle06/bookservice/service/BookServiceImpl.java
@@ -0,0 +1,55 @@
+package com.aivle06.bookservice.service;
+
+import com.aivle06.bookservice.domain.Book;
+import com.aivle06.bookservice.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService{
+
+    @Autowired
+    private final BookRepository bookRepository;
+
+    //저장
+    @Override
+    public Book createBook(Book book){
+        return bookRepository.save(book);
+    }
+
+    //전체 조회
+    @Override
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    //단건 조회
+    @Override
+    public Book getBookById(Long id){
+        return bookRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("책이 존재하지 않습니다."));
+    }
+
+    //업데이트
+    @Override
+    public Book updateBook(Long id, Book book){
+        Book b = getBookById(id);
+        b.setTitle(book.getTitle());
+        b.setAuthor(book.getAuthor());
+        b.setImage_url(book.getImage_url());
+
+        return bookRepository.save(b);
+
+    }
+
+    //삭제
+    public void deleteBook(Long id){
+        Book b = getBookById(id);
+        bookRepository.delete(b);
+    }
+
+
+}
